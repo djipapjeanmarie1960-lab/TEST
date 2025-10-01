17575f02a05571601ac7b7d02548ffd539b170db
djipapjeanmarie1960-lab <djipapjeanmarie1960@gmail.com>
2025-09-30 22:18:13 -0400

    tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
